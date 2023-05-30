d5536e22a12932ac0d0e5e582cac2b468cfb222c
Hosein <byt.hossein.bayati@gmail.com>
2023-05-30 19:40:18 +0330

    first commit | syscalls and test user program added

---
+++ xv6-public-master/sematest.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(void) 
+{
+printf(1, "Note: Unix V6 was released in year %d\n", sem_acquire());
+printf(1, "Note: Unix V6 was released in year %d\n", sem_release());
+printf(1, "Note: Unix V6 was released in year %d\n", sem_init());
+
+    exit();
+} 
